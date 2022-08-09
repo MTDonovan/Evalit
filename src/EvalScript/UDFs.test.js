8e53cdb29a72596b83dec72cdb1f0b12b966c71e
Matthew Donovan <matthew.donovan@fieldcap.ca>
2022-08-09 11:14:51 -0600

    09-08-2022

--- src/EvalScript/UDFs.test.js
+++ src/EvalScript/UDFs.test.js
@@ -6,5 +6,5 @@ test(`"2.586 . fix {2}" shall output "2.59"`, () => {
 });
 
 test(`"10 . add {20, 30}" shall output "60"`, () => {
-  expect(runsec("10 . add {20, 30}").out).toBe("60");
+  expect(runsec("5 . add {10, 20, 30}").out).toBe("65");
 });
