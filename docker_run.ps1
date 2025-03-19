089577325dbeceb48c1155403539aae60c6a6d2b
MTD <MTDonovan@outlook.com>
2025-03-18 18:57:49 -0600

    2025-03-18 18:57:49.148474

---
+++ docker_run.ps1
@@ -0,0 +1,15 @@
+docker run -d --rm --name evalit-container:v1 evalit-image:v1
+
+sudo apt-get update && \
+    apt-get install -y \
+    libnss3 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libcups2 \
+    libxkbcommon-x11-0 \
+    libxcomposite1 \
+    libxrandr2 \
+    libgbm1 \
+    libpango-1.0-0 \
+    libpangocairo-1.0-0 \
+    libasound2
